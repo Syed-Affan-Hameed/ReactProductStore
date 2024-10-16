c88c2ee3b49285d788c8cc6f15b444360577a340
Syed Affan Hameed <syedaffanhameed@gmail.com>
2024-10-16 22:25:14 +0530

    removed local host

--- backend/server.js
+++ backend/server.js
@@ -3,39 +3,40 @@ import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import productRoutes from "./Routes/products.route.js";
 import cors from "cors";
-import path from 'path';
+import path from "path";
 
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 5000;
-const __dirname =path.resolve();
+const __dirname = path.resolve();
 app.use(express.json()); // middleware: allows us to work with json
 // cors settings to allow frontend application to hit the backend server.
-const allowedOrigins = ['http://localhost:5173', 'https://reactproductstore.onrender.com'];
+const allowedOrigins = ["https://reactproductstore.onrender.com"];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Check if the incoming request origin is in the allowedOrigins array
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
-app.options('*', cors());  // Allow preflight for all routes
-//for all the routes starting with '/api/products' we are sending it to productRoutes
-app.use('/api/products',productRoutes);
-
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")));
-  // for any other route other than backend ones.
-  app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Check if the incoming request origin is in the allowedOrigins array
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
-}
+);
+app.options("*", cors()); // Allow preflight for all routes
+//for all the routes starting with '/api/products' we are sending it to productRoutes
+app.use("/api/products", productRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  // for any other route other than backend ones.
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // Starts the Server
 app.listen(port, () => {
